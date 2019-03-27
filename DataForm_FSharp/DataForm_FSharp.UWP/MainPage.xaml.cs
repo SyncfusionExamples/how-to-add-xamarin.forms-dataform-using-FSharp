47b6a66377ea5a74da89b82160888ef50d66c4ee
Karthik Raja Arumugam <karthikraja.arumugam@syncfusion.com>
2019-03-27 15:19:31 +0530

    DataForm using FSharp

---
+++ DataForm_FSharp/DataForm_FSharp.UWP/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.XForms.UWP.DataForm;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace DataForm_FSharp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            SfDataFormRenderer.Init();
+            LoadApplication(new DataForm_FSharp.App());
+        }
+    }
+}
---
+++ DataForm_FSharp/DataForm_FSharp/DataForm_FSharp.fs
@@ -0,0 +1,37 @@
+﻿namespace DataForm_FSharp
+
+open System
+open System.Numerics
+open Xamarin.Forms
+open Xamarin.Forms.Xaml
+open Syncfusion.XForms.DataForm
+
+
+type LoginRow() =     
+
+    member val user = "" with get,set
+    member val pwd = "" with get,set
+    member val zone = "" with get,set
+    member val url = "" with get,set
+
+type TwoButtonsPage() = 
+    inherit ContentPage()
+
+    let mainStack = StackLayout()
+    do mainStack.BackgroundColor <- Color.Orange
+            
+    do mainStack.VerticalOptions <- LayoutOptions.FillAndExpand  
+    do mainStack.Orientation <- StackOrientation.Vertical
+   
+      
+    let dataForm = new SfDataForm()
+    do  dataForm.BackgroundColor <- Color.Red
+    do  dataForm.DataObject <- new LoginRow()
+    
+    do mainStack.Children.Add(dataForm)       
+    
+    do base.Content <- mainStack
+
+ 
+  
+      
